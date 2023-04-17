ef06d9f4dd355aa3ad6257875eb144a8489d0a7e
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-17 15:29:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,55 @@
 provider "aws" {
-  access_key = "<your_access_key>"
-  secret_access_key = "<your_secret_access_key>"
-  region = "<aws_region>"
+  region = var.aws_region
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "my_subnet" {
-  cidr_block = "10.0.1.0/24"
-  vpc_id = aws_vpc.my_vpc.id
-}
-
+#Create security group with firewall rules
 resource "aws_security_group" "my_security_group" {
-  name_prefix = "my-security-group"
+  name        = var.security_group
+  description = "security group for Ec2 instance"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = var.security_group
   }
 }
 
-resource "aws_instance" "my_ec2_instance" {
-  ami = "ami-0c55b159cb2c71c99"
-  instance_type = "t2.micro"
-  key_name = "my-key-pair"
-  subnet_id = aws_subnet.my_subnet.id
-  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  key_name = var.key_name
+  instance_type = var.instance_type
+  security_groups= [var.security_group]
+  tags= {
+    Name = var.tag_name
+  }
+}
 
-  tags = {
-    Name = "my-ec2-instance"
+# Create Elastic IP address
+resource "aws_eip" "myFirstInstance" {
+  vpc      = true
+  instance = aws_instance.myFirstInstance.id
+tags= {
+    Name = "my_elastic_ip"
   }
 }
