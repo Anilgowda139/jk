8f9f40b02e615f71473db79ea0a57f681b1c5c01
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-19 16:58:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,66 +34,6 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-# Create the VPC
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_cidr_block
-  tags = {
-    Name = var.vpc_name
-  }
-}
-
-# Create the internet gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = var.internet_gateway_name
-  }
-}
-
-# Create the public subnet
-resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr_block
-  availability_zone = var.public_subnet_availability_zone
-  tags = {
-    Name = var.public_subnet_name
-  }
-}
-
-# Create the private subnet
-resource "aws_subnet" "private" {
-  vpc_id = aws_vpc.main.id
-  cidr_block = var.private_subnet_cidr_block
-  availability_zone = var.private_subnet_availability_zone
-  tags = {
-    Name = var.private_subnet_name
-  }
-}
-
-# Create the route table
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = var.route_table_name
-  }
-}
-
-# Associate the public subnet with the route table
-resource "aws_route_table_association" "public" {
-  subnet_id = aws_subnet.public.id
-  route_table_id = aws_route_table.main.id
-}
-
-# Associate the private subnet with the route table
-resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.private.id
-  route_table_id = aws_route_table.main.id
-}
-
 # Create AWS ec2 instance
 resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
