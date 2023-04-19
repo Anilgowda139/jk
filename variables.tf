012e53b587511f9d7acf10352a9fa77149733005
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-19 16:37:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,48 @@ variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
   default     = "ami-02eb7a4783e7e9317"
 }
+
+variable "vpc_cidr_block" {
+  description = "The CIDR block for the VPC"
+  default = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "The name of the VPC"
+  default = "my-vpc"
+}
+
+variable "internet_gateway_name" {
+  description = "The name of the internet gateway"
+  default = "my-internet-gateway"
+}
+
+variable "public_subnet_cidr_block" {
+  description = "The CIDR block for the public subnet"
+  default = "10.0.1.0/24"
+}
+
+variable "public_subnet_availability_zone" {
+  description = "The availability zone for the public subnet"
+  default = "us-west-2a"
+}
+
+variable "public_subnet_name" {
+  description = "The name of the public subnet"
+  default = "my-public-subnet"
+}
+
+variable "private_subnet_cidr_block" {
+  description = "The CIDR block for the private subnet"
+  default = "10.0.2.0/24"
+}
+
+variable "private_subnet_availability_zone" {
+  description = "The availability zone for the private subnet"
+  default = "us-west-2b"
+}
+
+variable "private_subnet_name" {
+  description = "The name of the private subnet"
+  default = "my-private-subnet
+}
