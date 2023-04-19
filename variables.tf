a6efe9d7b0380db1a9cd507f21c4a44921d054bd
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-19 22:13:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -27,3 +27,52 @@ variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
   default     = "ami-02eb7a4783e7e9317"
 }
+
+# Provider configuration for AWS
+provider "aws" {
+  region = "ap-south-1" # Change to your desired AWS region
+}
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16" # Change to your desired VPC CIDR block
+
+  tags = {
+    Name = "My VPC"
+  }
+}
+
+# Create a public subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24" # Change to your desired public subnet CIDR block
+  availability_zone = "us-east-1a" # Change to your desired availability zone
+
+  tags = {
+    Name = "Public Subnet"
+  }
+}
+
+# Create a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24" # Change to your desired private subnet CIDR block
+  availability_zone = "us-east-1b" # Change to your desired availability zone
+
+  tags = {
+    Name = "Private Subnet"
+  }
+}
+
+# Output the VPC and subnet IDs
+output "vpc_id" {
+  value = aws_vpc.my_vpc.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
