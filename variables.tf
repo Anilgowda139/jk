6c7a0f31c2056d0d3c9bd7bb3a867e703844af79
Anil Gowda H <109986253+Anilgowda139@users.noreply.github.com>
2023-04-20 09:57:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "us-west-1"
 }
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "web-1"
+  default     =  "web-2"
 }
 
 variable "instance_type" {
